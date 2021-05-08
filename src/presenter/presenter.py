0e58d5aacab480a6cb97010706f29fde2c0523fc
hzume <yota040@gmail.com>
2021-05-09 01:29:08 +0900

    やりかけ

--- src/presenter/presenter.py
+++ src/presenter/presenter.py
@@ -1,17 +1,35 @@
 import PySimpleGUI as sg
+import openpyxl
+import msoffcrypto
+import tempfile
 from src.views.view import InterFace
-from src.models.model import Tutor, Class
+from src.models.model import Tutor, Tutors, Class
+from src.settings.translate import *
 
 class Presenter:
     def __init__(self, window: sg.Window):
         self.window = window
-        self.tutor = {}
+        self.tutors = Tutors()
+    
+    # ---to Model---
+    def tutors_init(self, params_list):
+        for params in params_list:
+            self.tutors.add(Tutor(**params))
+    
+    def 
 
-    # --------to Model--------
-    def new_tutor(self, name, fullname, pay_class, pay_officework, trans_fee):
-        self.tutor[name] = Tutor(name, fullname, pay_class, pay_officework, trans_fee)
-    
-    # --------from Model--------
-    
-    # --------to View--------
+    # ---from View---
+    def get_tutors(self, tutor_path):
+            wb = openpyxl.load_workbook(tutor_path)
+            ws = wb.worksheets[0]
+            params_list = []
+            for row in ws.rows:
+                if row[0].row == 1:
+                    keys = row
+                else:
+                    params = {}
+                    for k, v in zip(keys, row):
+                        params[ja_to_en[k.value]] = v.value
+                    params_list.append(params)
+            return params_list
     
---
+++ src/settings/translate.py
@@ -0,0 +1,15 @@
+ja_to_en = {
+    "名字": "name",
+    "フルネーム": "fullname",
+    "授業給": "pay_class",
+    "事務給": "pay_office_work",
+    "交通費": "trans_fee"
+}
+
+en_to_ja = {
+    "name": "名字",
+    "fullname": "フルネーム",
+    "pay_class": "授業給",
+    "pay_office_work": "事務給",
+    "trans_fee": "交通費"
+}
