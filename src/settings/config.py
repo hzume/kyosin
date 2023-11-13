aeff860b5d311aa318f6074ae18e651b6836aea9
hzume <yota040@gmail.com>
2023-11-13 17:18:21 +0900

    a

--- src/settings/config.py
+++ src/settings/config.py
@@ -1,9 +1,9 @@
 default = {
     'password': 1219,
     'tutor_path': 'C:/Users/owner/OneDrive/デスクトップ/給与明細作成/講師情報.xlsx',
-    'admin_path': 'C:/Users/owner/Downloads/2022　10月時間割表 - コピー.xlsx',
+    'admin_path': 'C:/Users/owner/Downloads/20239月分給与計算.xlsx',
     'template_path': 'C:/Users/owner/OneDrive/デスクトップ/給与明細作成/テンプレート.xlsx',
-    'year': 2022,
-    'output_folder': 'C:/Users/owner/OneDrive/デスクトップ/給与明細作成/出力先フォルダ最新',
+    'year': 2023,
+    'output_folder': 'C:/Users/owner/OneDrive/デスクトップ/給与明細作成/出力先フォルダ２０２３　',
     'gensen_path': 'C:/Users/owner/OneDrive/デスクトップ/給与明細作成/源泉徴収税額表.xlsx'
 }
